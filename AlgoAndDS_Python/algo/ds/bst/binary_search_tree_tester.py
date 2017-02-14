def1032169ac9ccf9aa8e7c0239a0c6a2d8504bf
sachin <sachin@sachin>
2017-02-14 20:27:51 +0530

    binary search tree operations added.

--- AlgoAndDS_Python/algo/ds/bst/binary_search_tree_tester.py
+++ AlgoAndDS_Python/algo/ds/bst/binary_search_tree_tester.py
@@ -7,8 +7,17 @@ def close():
     exit()
     
 def insert():
-    data = int(input("Enter data: "))
-    bst.insert(data)
+    try:
+        data = int(input("Enter data: "))
+        bst.insert(data)
+    except ValueError:
+        print("Invalid input, operation unsuccessful...")
+
+def insert_multiple():
+    all_values = input("Enter all values (comma(,) separated): ")
+    all_values_arr = all_values.split(",")
+    for value in all_values_arr:
+        bst.insert(int(value))
 
 def in_order_tree_walk():
     bst.tree_walk("IN")
@@ -21,7 +30,10 @@ def post_order_tree_walk():
 
 def search_data():
     data = int(input("Enter data: "))
-    bst.search_data(data)
+    if bst.search_data(data):
+        print("data found")
+    else:
+        print("data not found")
 
 def remove_data():
     data = int(input("Enter data: "))
@@ -35,47 +47,59 @@ def predecssor():
     data = int(input("Enter data: "))
     bst.predecssor(data)
 
-bst.insert(21)
+bst.insert(25)
+bst.insert(11)
 bst.insert(13)
-bst.insert(52)
-bst.insert(5)
-bst.insert(17)
-bst.insert(22)
-bst.insert(15)
 bst.insert(12)
-bst.insert(1)
-bst.insert(3)
+bst.insert(51)
+bst.insert(61)
+bst.insert(81)
+bst.insert(72)
 bst.insert(99)
-bst.insert(97)
-bst.insert(44)
-bst.insert(40)
+bst.insert(54)
+bst.insert(1)
+bst.insert(7)
 bst.insert(14)
-bst.insert(19)
-bst.insert(2)
+bst.insert(21)
+bst.insert(27)
+bst.insert(92)
+bst.insert(9)
+bst.insert(29)
+bst.insert(71)
+bst.insert(91)
+bst.insert(94)
 
 options = {
         0: close,
         1: insert,
-        2: in_order_tree_walk,
-        3: pre_order_tree_walk,
-        4: post_order_tree_walk,
-        5: search_data,
-        6: remove_data,
-        7: successor,
-        8: predecssor
+        2: insert_multiple,
+        3: in_order_tree_walk,
+        4: pre_order_tree_walk,
+        5: post_order_tree_walk,
+        6: search_data,
+        7: remove_data,
+        8: successor,
+        9: predecssor
     }
 
 string = """            0: Exit
             1: Insert
-            2: In order tree walk
-            3: Pre order tree walk
-            4: Post order tree walk
-            5: Search Data
-            6: Remove Data
-            7: Successor
-            8: Predecessor
+            2: Insert Multiple
+            3: In order tree walk
+            4: Pre order tree walk
+            5: Post order tree walk
+            6: Search Data
+            7: Remove Data
+            8: Successor
+            9: Predecessor
             \nEnter your option: """
 while(True):
-    operation_index = int(input(string))
-    options[operation_index]()
+    try:
+        operation_index = int(input(string))
+        if operation_index < 0 or operation_index > 9:
+            raise ValueError()
+        options[operation_index]()
+    except ValueError:
+        print("Invalid input, please enter valid input...")
+    
     
--- AlgoAndDS_Python/tester/tester.py
+++ AlgoAndDS_Python/tester/tester.py
@@ -1,1 +1,14 @@
-print("Hello World")
+class TestClass:
+    __privateField = 33
+    def __init__(self):
+#         self.__privateField = 4
+        self.publicField = 5
+        
+    def print_all(self): 
+        print(self.__privateField)
+        print(self.publicField)
+        
+tester = TestClass()
+tester.print_all()
+print(tester.publicField)
+print(tester.__privateField)
