e97a4c976ad33b798b50d1111f84c7ff187f4e5f
sachin <sachin@sachin>
2017-02-19 19:54:01 +0530

    dynamic programming: rod cutting... added.

--- AlgoAndDS_Python/algo/dynamic_programming/rod_cut_dynamic_programming.py
+++ AlgoAndDS_Python/algo/dynamic_programming/rod_cut_dynamic_programming.py
@@ -1,33 +1,54 @@
-import random
+# import random
 
 class RodCutDynamicProgramming:
 
-    def memoized_cut_rod(self, prices, size_of_rod):
-        r = [];
-        for _ in range(len(prices)):
-            r.append(-1)
-            
-        return self.__memoized_cut_rod_aux(prices, size_of_rod, r)  
-        
-    def __memoized_cut_rod_aux(self, prices, size_of_rod, r):
-        if r[size_of_rod] != -1:
-            return r[size_of_rod]
-        q = 0;
-        if size_of_rod != 0:
-            q = -1
-            for i in range(1, size_of_rod + 1):
-                q = max(q, prices[i] + self.__memoized_cut_rod_aux(prices, size_of_rod-i, r))
-        r[size_of_rod] = q
-        return q
-
-
-MAX = 100
-prices = [0]
-for i in range(1, MAX):
-    prices.append(prices[i-1] + random.randint(2,10))
+    def __init__(self):
+        self.MIN_VALUE = -32767
     
-print(*prices, sep=",")
-print("\n\n")
+    def memoized_cut_rod(self, prices, size):
+        values = [self.MIN_VALUE for _ in range(size+1)];
+        return self.__memoized_cut_rod_aux(prices, size, values)  
+        
+    def __memoized_cut_rod_aux(self, prices, size, values):
+        
+        if values[size] != self.MIN_VALUE:
+            return values[size]
+        max_value = 0;
+        if size == 0:
+            max_value = 0
+        else:
+            max_value = self.MIN_VALUE
+            for i in range(1, size+1):
+                print(size)
+                max_value = max(max_value, prices[i] + self.__memoized_cut_rod_aux(prices, size-i, values))
+        values[size] = max_value
+        return max_value
+    
+    def buttom_up_cut_rod(self, prices, ize):
+        
+        temp = [0] * (size + 1)
+        for i in range(1, size):
+            max_val = self.MIN_VALUE
+            for j in range(i):
+                max_val = max(max_val, prices[j+1] + temp[i - j - 1])
+            print(max_val)
+            temp[i] = max_val
+        return temp[size-1]
+        
+
+# MAX = 100
+# prices = [0]
+# for i in range(1, MAX):
+#     prices.append(prices[i-1] + random.randint(2,10))
+#     
+# print(*prices, sep=",")
+# print("\n\n")
+
+prices = [0,1,5,8,9,10,17,17,20]
+size = len(prices)
     
 rcdp = RodCutDynamicProgramming()
-print(rcdp.memoized_cut_rod(prices, 5))
+print("Memoized: " + str(rcdp.memoized_cut_rod(prices, size-1)))
+print("Bottom up: " + str(rcdp.buttom_up_cut_rod(prices, size)))
+
+""" TODO: this may require some modifications. Have to look later. """
--- AlgoAndDS_Python/algo/dynamic_programming/rod_cut_soln_using_recursion.py
+++ AlgoAndDS_Python/algo/dynamic_programming/rod_cut_soln_using_recursion.py
@@ -1,17 +1,22 @@
-import random;
-prices = [0]
-for i in range(1, 100):
-    prices.append(prices[i-1] + random.randint(5,20))
+# import random;
+# prices = [0]
+# for i in range(1, 100):
+#     prices.append(prices[i-1] + random.randint(5,20))
+# from algo.dynamic_programming.rod_cut_dynamic_programming import prices
     
 # print(*prices, sep=",")
 
+prices = [0,1,5,8,9,10,17,17,20]
+size = len(prices)
+
+INT_MIN = -32767
 def cut_rod(prices, length = 6):
     if length == 0:
         return 0
-    q = -1
+    q = INT_MIN
     for cntr in range(1, length+1):
         q = max(q, prices[cntr] + cut_rod(prices, length - cntr))
     print(q)
     return q
     
-print("Max profit: " + str(cut_rod(prices, 10)))
+print("\nMax profit: " + str(cut_rod(prices, 4)))
